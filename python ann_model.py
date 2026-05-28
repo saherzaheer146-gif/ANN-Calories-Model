6b9851ac88d8c34140b1bce951de09c04d074a9d
saherzaheer146-gif <saherzaheer146@gmail.com>
2026-05-29 02:57:58 +0500

    Create python ann_model.py

---
+++ python ann_model.py
@@ -0,0 +1,9 @@
+   # ANN Calories Model
+   
+   Ye ANN model calories dataset pe train kiya gaya hai.
+   
+   **Files:**
+   - ann_model.py → Model code
+   - calories.csv → Dataset
+   
+   **Run karne ka tareeka:**
